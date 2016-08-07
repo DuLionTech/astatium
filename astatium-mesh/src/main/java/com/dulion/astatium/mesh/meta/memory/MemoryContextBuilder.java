3a387e0f938ae3ebddf3a0858982854f8c6440a0
XingLion <XingLion@10.1.10.185>
2016-08-06 23:37:02 -0700

    Adding checkstyle and findbugs. Adjusting styling.

--- astatium-mesh/src/main/java/com/dulion/astatium/mesh/meta/memory/MemoryContextBuilder.java
+++ astatium-mesh/src/main/java/com/dulion/astatium/mesh/meta/memory/MemoryContextBuilder.java
@@ -1,19 +1,25 @@
 /**
  * Copyright 2016 Phillip DuLion
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
+
 package com.dulion.astatium.mesh.meta.memory;
+
+import com.dulion.astatium.mesh.Context;
+import com.dulion.astatium.mesh.meta.ChildData;
+import com.dulion.astatium.mesh.meta.ContextBuilder;
+import com.dulion.astatium.mesh.meta.ContextData;
+import com.dulion.astatium.mesh.meta.ContextType;
+import com.dulion.astatium.mesh.meta.EdgeData;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,107 +29,88 @@ import java.util.function.BiFunction;
 
 import javax.xml.namespace.QName;
 
-import com.dulion.astatium.mesh.Context;
-import com.dulion.astatium.mesh.meta.ChildData;
-import com.dulion.astatium.mesh.meta.ContextBuilder;
-import com.dulion.astatium.mesh.meta.ContextData;
-import com.dulion.astatium.mesh.meta.ContextType;
-import com.dulion.astatium.mesh.meta.EdgeData;
 
+public class MemoryContextBuilder implements ContextBuilder {
+  private static final String CHILD_ELEMENT_PREFIX = "element:";
+  private static final String CHILD_ATTRIBUTE_PREFIX = "attribute:";
 
-public class MemoryContextBuilder implements ContextBuilder
-{
-	private static final String CHILD_ELEMENT_PREFIX = "element:";
-	private static final String CHILD_ATTRIBUTE_PREFIX = "attribute:";
+  private final List<Map<String, ChildData>> children = new ArrayList<>();
 
-	private final List<Map<String, ChildData>> children = new ArrayList<>();
-	
-	public MemoryContextBuilder()
-	{
-		children.add(new HashMap<>());
-	}
+  public MemoryContextBuilder() {
+    children.add(new HashMap<>());
+  }
 
-	@Override
-	public Builder builder()
-	{
-		return new Instance();
-	}
+  @Override
+  public Builder builder() {
+    return new Instance();
+  }
 
-	private class Instance implements Builder
-	{
-		private Context parent;
+  private class Instance implements Builder {
+    private Context parent;
 
-		private ContextType type;
+    private ContextType type;
 
-		private QName name;
+    private QName name;
 
-		private BiFunction<ContextData, EdgeData, Context> callback;
+    private BiFunction<ContextData, EdgeData, Context> callback;
 
-		@Override
-		public Builder withParent(Context parent)
-		{
-			this.parent = parent;
-			return this;
-		}
+    @Override
+    public Builder withParent(Context parent) {
+      this.parent = parent;
+      return this;
+    }
 
-		@Override
-		public Builder withType(ContextType type)
-		{
-			this.type = type;
-			return this;
-		}
+    @Override
+    public Builder withType(ContextType type) {
+      this.type = type;
+      return this;
+    }
 
-		@Override
-		public Builder withName(QName name)
-		{
-			this.name = name;
-			return this;
-		}
+    @Override
+    public Builder withName(QName name) {
+      this.name = name;
+      return this;
+    }
 
-		@Override
-		public Builder withCallback(BiFunction<ContextData, EdgeData, Context> callback)
-		{
-			this.callback = callback;
-			return this;
-		}
+    @Override
+    public Builder withCallback(BiFunction<ContextData, EdgeData, Context> callback) {
+      this.callback = callback;
+      return this;
+    }
 
-		@Override
-		public Context build()
-		{
-			ChildData child = childData();
+    @Override
+    public Context build() {
+      ChildData child = childData();
 
-			// @formatter:off
-			ContextData contextData = ContextData.builder()
-					.contextId(child.getContextId())
-					.namespace(name.getNamespaceURI())
-					.name(name.getLocalPart())
-					.type(type)
-					.build();
+      ContextData contextData = ContextData.builder()
+          .contextId(child.getContextId())
+          .namespace(name.getNamespaceURI())
+          .name(name.getLocalPart())
+          .type(type)
+          .build();
 
-			EdgeData edgeData = EdgeData.builder()
-					.parentId(parent.getContextId())
-					.childId(child.getContextId())
-					.index(child.getIndex())
-					.build();
-			// @formatter:on
+      EdgeData edgeData = EdgeData.builder()
+          .parentId(parent.getContextId())
+          .childId(child.getContextId())
+          .index(child.getIndex())
+          .build();
 
-			return callback.apply(contextData, edgeData);
-		}
+      return callback.apply(contextData, edgeData);
+    }
 
-		private ChildData childData()
-		{
-			Map<String, ChildData> childMap = children.get(parent.getContextId());
-			String childKey = ((type == ContextType.ATTRIBUTE) ? CHILD_ATTRIBUTE_PREFIX : CHILD_ELEMENT_PREFIX) + name;
+    private ChildData childData() {
+      Map<String, ChildData> childMap = children.get(parent.getContextId());
+      String childKey =
+          ((type == ContextType.ATTRIBUTE) ? CHILD_ATTRIBUTE_PREFIX : CHILD_ELEMENT_PREFIX) + name;
 
-			ChildData child = childMap.get(childKey);
-			if (null == child)
-			{
-				child = ChildData.builder().index(childMap.size()).contextId(children.size()).build();
-				childMap.put(childKey, child);
-				children.add(new HashMap<>());
-			}
+      ChildData child = childMap.get(childKey);
+      if (null == child) {
+        child = ChildData.builder().index(childMap.size()).contextId(children.size()).build();
+        childMap.put(childKey, child);
+        children.add(new HashMap<>());
+      }
 
-			return child;
-		}
-	}
+      return child;
+    }
+  }
 }
--- astatium-mesh/src/main/java/com/dulion/astatium/mesh/meta/memory/MemoryContextLoader.java
+++ astatium-mesh/src/main/java/com/dulion/astatium/mesh/meta/memory/MemoryContextLoader.java
@@ -1,49 +1,43 @@
 /**
  * Copyright 2016 Phillip DuLion
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
-package com.dulion.astatium.mesh.meta.memory;
 
-import java.util.function.BiConsumer;
+package com.dulion.astatium.mesh.meta.memory;
 
 import com.dulion.astatium.mesh.meta.ContextData;
 import com.dulion.astatium.mesh.meta.ContextLoader;
 import com.dulion.astatium.mesh.meta.EdgeData;
 
-public class MemoryContextLoader implements ContextLoader
-{
-	@Override
-	public Loader loader()
-	{
-		return new Instance();
-	}
-	
-	private class Instance implements Loader
-	{
-		@Override
-		public Loader withCallback(BiConsumer<ContextData, EdgeData> callback)
-		{
-			return this;
-		}
+import java.util.function.BiConsumer;
 
-		/** 
-		 * @see com.expedia.fcts.booking.sync.shred.redis.Loader#load()
-		 */
-		@Override
-		public void load()
-		{
-			// Do nothing
-		}
-	}
+public class MemoryContextLoader implements ContextLoader {
+  @Override
+  public Loader loader() {
+    return new Instance();
+  }
+
+  private static class Instance implements Loader {
+    @Override
+    public Loader withCallback(BiConsumer<ContextData, EdgeData> callback) {
+      return this;
+    }
+
+    /**
+     * @see com.expedia.fcts.booking.sync.shred.redis.Loader#load()
+     */
+    @Override
+    public void load() {
+      // Do nothing
+    }
+  }
 }
